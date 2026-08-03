326578e26ec5d06a2dd6c3ec2e4a86ed97c3fc9c
Xiaozhi Liu <165480625+xzliu-opt@users.noreply.github.com>
2026-08-03 16:16:09 +0800

    [KernelGen][hygon] Add specialized log_normal_ kernel (#217)

--- src/flag_gems/runtime/backend/_hygon/ops/__init__.py
+++ src/flag_gems/runtime/backend/_hygon/ops/__init__.py
@@ -30,6 +30,7 @@ from .gelu import gelu, gelu_
 from .hadamard_transform import hadamard_transform
 from .index_copy_ import index_copy, index_copy_
 from .isin import isin
+from .log_normal_ import log_normal_
 from .matmul_bf16 import matmul_bf16
 from .matmul_int8 import matmul_int8
 from .max_pool3d_with_indices import max_pool3d_backward, max_pool3d_with_indices
@@ -78,6 +79,7 @@ __all__ = [
     "index_copy",
     "index_copy_",
     "isin",
+    "log_normal_",
     "matmul_bf16",
     "matmul_int8",
     "max_pool3d_backward",
