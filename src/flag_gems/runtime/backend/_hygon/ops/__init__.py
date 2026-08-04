01f901aacc9bdb4c5c4db9adaf4a5c3cfb1dfc30
Xiaozhi Liu <165480625+xzliu-opt@users.noreply.github.com>
2026-08-04 21:12:55 +0800

    [KernelGen][Hygon] Add _amp_foreach_non_finite_check_and_unscale_ Hygon specialized operator (#224)

--- src/flag_gems/runtime/backend/_hygon/ops/__init__.py
+++ src/flag_gems/runtime/backend/_hygon/ops/__init__.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from ._amp_foreach_non_finite_check_and_unscale_ import (
+    _amp_foreach_non_finite_check_and_unscale_,
+)
 from .adaptive_max_pool3d_backward import adaptive_max_pool3d_backward
 from .any import any, any_dim, any_dims
 from .attention import (
@@ -113,6 +116,7 @@ from .weight_norm import (
 )
 
 __all__ = [
+    "_amp_foreach_non_finite_check_and_unscale_",
     "_unique2",
     "adaptive_max_pool3d_backward",
     "avg_pool3d_backward",
