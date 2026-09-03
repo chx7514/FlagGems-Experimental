67afb5caeead39284326304f84628cbe9d75623a
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-09-03 13:01:22 +0800

    [KernelGen][MThreads] Add lcm Moore Threads specialized operator (#301)

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -47,6 +47,7 @@ from .index_add import index_add, index_add_
 from .index_copy_ import index_copy, index_copy_
 from .index_put import _index_put_impl_, index_put, index_put_
 from .index_select import index_select
+from .lcm import lcm
 from .linalg_cholesky import linalg_cholesky
 from .log import log
 from .log2_ import log2_
@@ -140,6 +141,7 @@ __all__ = [
     "index_put_",
     "_index_put_impl_",
     "index_select",
+    "lcm",
     "linalg_cholesky",
     "log",
     "log10",
