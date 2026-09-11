085ef7bcd6969905d90231f69b024a1addf7b9d4
chx7514 <hxchen7514@gmail.com>
2026-09-11 07:36:55 +0000

    [KernelGen][Thead] Add index_select_backward T-Head specialized operator

--- src/flag_gems/runtime/backend/_thead/ops/__init__.py
+++ src/flag_gems/runtime/backend/_thead/ops/__init__.py
@@ -31,6 +31,7 @@ from .embedding_dense_backward import embedding_dense_backward
 from .erfc import erfc
 from .gcd_ import gcd, gcd_
 from .index_copy_ import index_copy, index_copy_
+from .index_select_backward import index_select_backward
 from .lcm import lcm, lcm_
 from .linalg_cholesky import linalg_cholesky
 from .linalg_svdvals import linalg_svdvals
@@ -87,6 +88,7 @@ __all__ = [
     "gcd_",
     "index_copy",
     "index_copy_",
+    "index_select_backward",
     "lcm",
     "lcm_",
     "linalg_cholesky",
